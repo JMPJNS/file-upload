30d188df4e29fb8e3a7b9a9876a980febd138638
Jonas <jonas@jmp.blue>
2020-07-16 12:51:37 +0200

    test

--- ShareXUploadAPI/Controllers/ApiController.cs
+++ ShareXUploadAPI/Controllers/ApiController.cs
@@ -43,6 +43,7 @@ namespace ShareXUploadAPI.Controllers
         [HttpPost]
         public async Task<string> UploadImage()
         {
+            return "no";
             var re = Request;
             var apiKey = re.Headers.FirstOrDefault(x => x.Key.ToLower() == "x-api-key").Value;
 
