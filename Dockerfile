bd1e9bb2a9e02e8f1965e61700d71b573fbdae1c
Jonas <jonas@jmp.blue>
2020-07-16 10:58:58 +0200

    download files

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Build
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build-env
+WORKDIR /app
+
+COPY ShareXUploadAPI/*.csproj ./
+
+RUN dotnet restore
+
+COPY ShareXUploadAPI ./
+RUN dotnet publish -c Release -o out
+
+# Run
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 as run-env
+WORKDIR /app
+
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "ShareXUploadAPI.dll"]
--- ShareXUploadAPI/Controllers/ApiController.cs
+++ ShareXUploadAPI/Controllers/ApiController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using HeyRed.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,68 +14,114 @@ using Microsoft.Extensions.Logging;
 namespace ShareXUploadAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("")]
     public class ApiController : ControllerBase
     {
         private readonly ILogger<ApiController> _logger;
+        private readonly string _realKey;
+        private readonly string _storagePath;
+        private readonly string _url;
 
         public ApiController(ILogger<ApiController> logger)
         {
             _logger = logger;
-        }
+            var realKey = Environment.GetEnvironmentVariable("APIKEY");
 
-        [HttpGet]
-        public string Get()
-        {
-            return "File Upload API";
+            var storagePath = Environment.GetEnvironmentVariable("STORAGEPATH");
+            var url = Environment.GetEnvironmentVariable("URL");
+            
+            if (storagePath == null || url == null)
+            {
+                throw new Exception("Storage path or URL Env variable not set");
+            }
+
+            _realKey = realKey;
+            _url = url;
+            _storagePath = storagePath;
         }
 
         [HttpPost]
         public async Task<string> UploadImage()
         {
             var re = Request;
-
-            // Console.WriteLine("Headers:\n");
-            // foreach (var header in re.Headers)
-            // {
-            //     Console.WriteLine($"\t{header.Key}: {header.Value.ToString()}");
-            // }
-            
-            Console.WriteLine("\n\n");
             var apiKey = re.Headers.FirstOrDefault(x => x.Key.ToLower() == "x-api-key").Value;
 
-            var realKey = Environment.GetEnvironmentVariable("APIKEY");
-
-            var storagePath = Environment.GetEnvironmentVariable("STORAGEPATH");
-            var url = Environment.GetEnvironmentVariable("URL");
-
-            if (apiKey.ToString() != realKey)
+            if (apiKey.ToString() != _realKey)
             {
-                return "Invalid or no API Key provided (x-api-key header)";
+                throw new ArgumentException("Invalid or no API Key provided (x-api-key header)");
             }
 
             if (re.Form.Files.Count == 0)
             {
-                return "No File in Form Body";
+                throw new ArgumentException( "No File in Form Body");
             } 
             
             IFormFile file = re.Form.Files.First();
 
             byte[] content;
-
+            string hash;
             using (var ms = new MemoryStream())
             {
                 await file.CopyToAsync(ms);
                 content = ms.ToArray();
             }
 
-            var filename = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds().ToString();
+            using (var md5 = MD5.Create())
+            {
+                hash = BitConverter.ToString(md5.ComputeHash(content)).Replace("-","").ToLower();
+            }
+            
+            var timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds().ToString();
 
-            string path = Path.Combine(storagePath, $"{filename}");
+            string filename = _generateFilename();
+
+            filename += timestamp.Substring(timestamp.Length - 2);
+            filename += hash.Substring(0, 2);
+            
+            var extension = MimeTypesMap.GetExtension(file.ContentType);
+            filename += $".{extension}";
+
+            string path = Path.Combine(_storagePath, $"{filename}");
             
             await System.IO.File.WriteAllBytesAsync(path, content);
             
-            return $"{url}/{filename}";
+            return $"{_url}/{filename}";
+        }
+
+        private Random _random = new Random();
+        private string _generateFilename()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 4)
+                .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+
+        [HttpGet]
+        [Route("/{name}")]
+        public async Task<byte[]> DownloadImage()
+        {
+            var re = Request;
+            var filename = re.RouteValues["name"];
+            if (filename == null)
+            {
+                throw new ArgumentException("Filename cannot be empty");
+            }
+            string path = Path.Combine(_storagePath, $"{filename}");
+            try
+            {
+                var content = await System.IO.File.ReadAllBytesAsync(path);
+
+                if (content == null)
+                {
+                    throw new IOException("File not Found");
+                }
+
+                return content;
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new FileNotFoundException($"File {filename} not found");
+            }
         }
         
     }
