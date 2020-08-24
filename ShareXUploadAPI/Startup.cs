ce3b8efb1e1bb1c90c050e47f068d782b8abd014
Jonas <service@jmp.blue>
2020-08-24 13:15:43 +0200

    change not found message

--- ShareXUploadAPI/Startup.cs
+++ ShareXUploadAPI/Startup.cs
@@ -41,27 +41,50 @@ namespace ShareXUploadAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else
             {
                 app.UseExceptionHandler(errorApp =>
                 {
                     errorApp.Run(async context =>
                     {
+                        var exceptionHandlerPathFeature = 
+                            context.Features.Get<IExceptionHandlerPathFeature>();
+                        
                         context.Response.StatusCode = 500;
                         context.Response.ContentType = "text/html";
 
-                        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
-                        await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+                        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                        {
+                            context.Response.StatusCode = 404;
+
+                            var sendText = "<html lang='en'>"+
+                                            "<head><style>"+
+                                            "h1 {text-align: center;}"+
+                                            "p {text-align: center;}"+
+                                            "div {text-align: center;}"+
+                                            "img {text-align: center; display: block; margin-left: auto; margin-right: auto;}"+
+                                            "</style></head>"+
+                                            "<body>"+
+                                            "<img src='https://cdn.jmp.blue/1OYM24bc.png'/>"+
+                                            $"<p>{exceptionHandlerPathFeature?.Error.Message}</p>"+
+                                            "</body></html>";
+
+                            await context.Response.WriteAsync(sendText);
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                            await context.Response.WriteAsync("ERROR!<br><br>\r\n");
                         
-                        var exceptionHandlerPathFeature = 
-                            context.Features.Get<IExceptionHandlerPathFeature>();
-                        await context.Response.WriteAsync(exceptionHandlerPathFeature?.Error.Message);
+                            await context.Response.WriteAsync(exceptionHandlerPathFeature?.Error.Message);
                         
-                        await context.Response.WriteAsync("</body></html>\r\n");
+                            await context.Response.WriteAsync("</body></html>\r\n");
+                        }
+                        
                         await context.Response.WriteAsync(new string(' ', 512)); // IE padding
                     });
                 });
